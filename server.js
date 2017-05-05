ecf7b645bc0fdbe4cbd2f137169c4d5556cf151b
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-05-04 20:00:21 -0400

    Get ipaddress, language, and operating system from request header to fill response json

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express')
 var app = express();
 
 app.get('/', function(req, res) {
-	var userIP = null;
-	var userLanguage = null;
-	var userOS = null;
+	var userIP = req.ip;
+	var userLanguage = req.headers['accept-language'].split(',')[0];
+	var userOS = req.headers['user-agent'].split('(')[1].split(')')[0];
 
 	var result = {
 		"ipAddress": userIP,
